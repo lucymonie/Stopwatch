2c4419f0e6c2c691ade8167152519729de067484
Lucy Monie <lucy.monie.hall@gmail.com>
2016-09-15 18:55:44 +0100

    adds hundredths of a second

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 var countMins = 0;
 var countSecs = 0;
+var countHunds = 0;
 var minutes = document.getElementById("minutes");
 var seconds = document.getElementById("seconds");
+var hundredths = document.getElementById("hund");
 var startButton = document.getElementById("start");
 var pauseButton = document.getElementById("pause");
 var resetButton = document.getElementById("reset");
@@ -22,26 +24,43 @@ var isPaused = true;
     isPaused = true;
     minutes.innerHTML = '00';
     seconds.innerHTML = '00';
+    hundredths.innerHTML = '000';
     countMins = 0;
     countSecs = 0;
+    countHunds = 0;
   });
 
-  var counter = setInterval(function() {
+  setInterval(function() {
     if(!isPaused) {
-      countSecs++;
-      if(countSecs < 10) {
-        seconds.innerHTML = '0' + countSecs;
-      } else if (countSecs >= 10 && countSecs < 60) {
-        seconds.innerHTML = countSecs;
-      } else if (countSecs === 60) {
-        seconds.innerHTML = '00';
-        minutesInc();
-        countSecs = 0;        
+      countHunds+=10;
+      if (countHunds < 10) {
+        hundredths.innerHTML = '0' + countHunds;
+      } else if (countHunds >= 10 && countHunds < 100) {
+        hundredths.innerHTML = '0' + countHunds;
+      } else if (countHunds >= 100 && countHunds < 1000) {
+        hundredths.innerHTML = countHunds;
+      } else if (countHunds===1000) {
+        secondsInc();
+        hundredths.innerHTML = '000';
+        countHunds = 0;      
       }
     }      
-  }, 1000);
+  }, 10);
 
-  var minutesInc = function() {
+ function secondsInc() {
+  countSecs++;
+    if(countSecs < 10) {
+      seconds.innerHTML = '0' + countSecs;
+    } else if(countSecs >= 10 && countSecs < 60) {
+      seconds.innerHTML = countSecs;
+    } else if(countSecs === 60) {
+      minutesInc();
+      seconds.innerHTML = '00';
+      countSecs = 0;
+    }
+  }
+
+  function minutesInc() {
   countMins++;
     if(countMins < 10) {
       minutes.innerHTML = '0' + countMins;
@@ -49,5 +68,6 @@ var isPaused = true;
       minutes.innerHTML = countMins;
     } else if(countMins === 60) {
       minutes.innerHTML = '00';
+      countMins = 0;
     }
   }
